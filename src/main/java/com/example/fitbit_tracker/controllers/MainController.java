c92a78a27e4870c17829a86778ec123d915eb9e6
Geetika-Jain1 <drgeetika332@gmail.com>
2024-09-23 18:53:48 +0200

    added scripts

--- src/main/java/com/example/fitbit_tracker/controllers/MainController.java
+++ src/main/java/com/example/fitbit_tracker/controllers/MainController.java
@@ -2,8 +2,9 @@ package com.example.fitbit_tracker.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MainController {
 
     @GetMapping("/")
